d5fc49e48e25a36eb3c6c90fd1a8d9ea56ecc0c2
caoyuanzhi <542727233@qq.com>
2021-02-06 22:48:58 +0800

    链接会导致执行的麻烦
    我宁愿修改后手工cp，也不想调试安装的时候麻烦。

--- bin/proxy-unset.sh
+++ bin/proxy-unset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 rm /tmp/.proxy-unset.sh||true
-sed    "s/-A/-D/g" ${PWD}/proxy-set.sh > /tmp/.proxy-unset.sh
+sed    "s/-A/-D/g" /usr/local/bin/proxy-set.sh > /tmp/.proxy-unset.sh
 sed -i "s/-N/-X/g" /tmp/.proxy-unset.sh
 sed -i "s/-xe/-x/g" /tmp/.proxy-unset.sh
 sed -i '2{H;d}; ${p;x;s/^\n//}' /tmp/.proxy-unset.sh
